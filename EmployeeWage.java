bcc938dcf29667499affa7ae59c3bb6ee5bfd572
Sudha Sahu <sudhasahu52@gmail.com>
2022-01-21 23:04:54 +0530

    seventh commit to calculate employee wages until maximum hours reached

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,13 +3,14 @@ public class EmployeeWage{
 	public static final int isParttime = 1;
 	public static final int isFulltime = 2;
 	public static final int empRatePerHr=20;
-	public static final int noOfWorkingDays=20;	
+	public static final int maxWorkingDays=20;
+	public static final int maxWorkingHrs=100;
 	public static void main(String args[])
 	{
 		int empHrs=0;
-		int empWage=0;
-		int empWageforMonth=0;
-		for(int day=0;day<noOfWorkingDays;day++) {
+		int noOfDays=0;
+		int noOfHrs=0;
+		while(noOfDays < maxWorkingDays && noOfHrs <= maxWorkingHrs) {
 
 			int empCheck =(int) Math.floor (Math.random() * 10) % 3;
 			switch (empCheck) {
@@ -22,11 +23,12 @@ public class EmployeeWage{
 			default:
 				empHrs=0;
 			}
-			empWage= empHrs * empRatePerHr;
-			empWageforMonth = empWageforMonth+empWage;
-			System.out.println("calculating EmpWage: "+ empWage);
+			noOfHrs=noOfHrs+empHrs;
+			noOfDays++;
+			System.out.println("Total Days: "+noOfDays +"Total no. of hrs: "+ noOfHrs);
 		}
-		System.out.println("calculating EmpWage for month: "+ empWageforMonth);
+		int empWageforMonth = noOfHrs*empRatePerHr;
+		System.out.println("total EmpWage for month: "+ empWageforMonth);
 	}
 
 }
