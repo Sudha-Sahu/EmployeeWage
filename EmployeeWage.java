f6da211704b728988c27e89b8a4d9cb1559834e8
Sudha Sahu <sudhasahu52@gmail.com>
2022-01-21 22:25:54 +0530

    second commit to check the employee is present or absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,11 @@
 public class EmployeeWage{
-	public static void main (String[] args){
-		System.out.println("Welcome to Employee Wage Computation!!!");
-
+	public static void main(String args[])
+	{
+		int isfulltime = 1;
+		double empCheck = Math.floor (Math.random() * 10) % 2;
+		if (empCheck == isfulltime)
+			System.out.println("Employee is Present");
+		else        
+			System.out.println("Employee is Absent");
 	}
 }
