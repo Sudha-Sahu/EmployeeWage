03ffa262097ecbc6faa761070af05891da0f3077
Sudha Sahu <sudhasahu52@gmail.com>
2022-01-21 22:57:38 +0530

    sixth commit to calculatethe wages for a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,25 +1,32 @@
 public class EmployeeWage{
+
+	public static final int isParttime = 1;
+	public static final int isFulltime = 2;
+	public static final int empRatePerHr=20;
+	public static final int noOfWorkingDays=20;	
 	public static void main(String args[])
 	{
-		int isParttime = 1;
-		int isFulltime = 2;
-		int empRatePerHr=20;
 		int empHrs=0;
-		int empWage=0;	
+		int empWage=0;
+		int empWageforMonth=0;
+		for(int day=0;day<noOfWorkingDays;day++) {
 
-		int empCheck =(int) Math.floor (Math.random() * 10) % 3;
-		switch (empCheck) {
-		case 1:
-			empHrs=4;
-			break;
-		case 2:
-			empHrs=8;
-			break;
-		default:
-			empHrs=0;
+			int empCheck =(int) Math.floor (Math.random() * 10) % 3;
+			switch (empCheck) {
+			case 1:
+				empHrs=4;
+				break;
+			case 2:
+				empHrs=8;
+				break;
+			default:
+				empHrs=0;
+			}
+			empWage= empHrs * empRatePerHr;
+			empWageforMonth = empWageforMonth+empWage;
+			System.out.println("calculating EmpWage: "+ empWage);
 		}
-		empWage= empHrs * empRatePerHr;
-		System.out.println("calculating EmpWage: "+empWage);
+		System.out.println("calculating EmpWage for month: "+ empWageforMonth);
 	}
 
 }
